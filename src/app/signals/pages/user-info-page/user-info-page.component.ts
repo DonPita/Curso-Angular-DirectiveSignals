d7350920e814560c81fe8ac51b32b28db840fe55
Adrian Pita <pitabermudez@gmail.com>
2024-05-23 17:01:11 +0200

    Señales de la información del personaje

--- src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
+import { UsersService } from '../../services/users-service.service';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './user-info-page.component.html',
   styleUrl: './user-info-page.component.css'
 })
-export class UserInfoPageComponent {
+export class UserInfoPageComponent implements OnInit {
 
+  private usersService = inject(UsersService);
+  public userId = signal(1);
+
+  //Usuario actual puede existir o no
+  public currentUser = signal<User | undefined>(undefined);
+  public userWasFound = signal(true);
+
+  ngOnInit(): void {
+    this.loadUser(this.userId());
+  }
+
+  loadUser(id: number) {
+    if (id <= 0) {
+      return;
+    }
+
+    this.userId.set(id);
+    this.currentUser.set(undefined); //Para que desaparezca entre cargas
+
+    this.usersService.getUserById(id)
+      .subscribe(user => {
+        this.currentUser.set(user)
+      });
+  }
 }
