98cca44b2ffa6c5c5b39f3ab1e1c3bfc9fcd427d
Adrian Pita <pitabermudez@gmail.com>
2024-05-23 11:22:01 +0200

    Taba todo mal creado, reconfigurando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductPageComponent } from './pages/product-page/product-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductPageComponent
   ],
   imports: [
     BrowserModule,
--- src/app/products/products-routing.module.ts
+++ src/app/products/products-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductPageComponent } from '../pages/product-page/product-page.component';
+import { ProductPageComponent } from '../products/pages/product-page/product-page.component';
 
 const routes: Routes = [
   {
