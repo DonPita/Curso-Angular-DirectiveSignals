def9ce003c2f6bc0d55f167ca43279eb0ba31a47
Adrian Pita <pitabermudez@gmail.com>
2024-05-24 08:30:37 +0200

    Preparacion para mutar, como los xmen

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -4,6 +4,12 @@ import { Component } from '@angular/core';
   templateUrl: './properties-page.component.html',
   styleUrl: './properties-page.component.css'
 })
+
 export class PropertiesPageComponent {
 
+
+  onFieldUpdated( field: string, value: string ) {
+
+    console.log({field, value});
+  }
 }
