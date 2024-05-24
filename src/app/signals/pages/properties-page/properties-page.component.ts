3cd3ffac97c00e5da0d45fc122a64f738124006c
Adrian Pita <pitabermudez@gmail.com>
2024-05-24 09:00:17 +0200

    Parte del codigo para actualizar el estado del usuario

--- src/app/signals/pages/properties-page/properties-page.component.ts
+++ src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { User } from '../../interfaces/user-request.interface';
 
 @Component({
   templateUrl: './properties-page.component.html',
@@ -7,9 +8,37 @@ import { Component } from '@angular/core';
 
 export class PropertiesPageComponent {
 
+  public user = signal<User>({
+    id: 2,
+    email: 'janet.weaver@reqres.in',
+    first_name: 'Janet',
+    last_name: 'Weaver',
+    avatar: 'https://reqres.in/img/faces/2-image.jpg'
+  })
 
-  onFieldUpdated( field: string, value: string ) {
 
-    console.log({field, value});
+  onFieldUpdated(field: keyof User, value: string) {
+
+    this.user.update(current => {
+      switch (field) {
+        case 'email':
+          current.email = value;
+          break;
+        case 'first_name':
+          current.first_name = value;
+          break;
+        case 'last_name':
+          current.last_name = value;
+          break;
+        case 'avatar':
+          current.avatar = value;
+          break;
+        case 'id':
+          current.id = Number(value);
+          break;
+      }
+
+      return {...current}; //Referencia al objeto original
+    })
   }
 }
